aa3e8356acb5137020f4b290a9cb30e5ce50b6b8
Linas Pelenis <lpelenis@gmail.com>
2020-11-25 15:44:32 +0200

    regional select added

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
 const display = document.getElementById('display');
+const selectRegion = document.getElementById('select');
+const regionBtn = document.getElementById('regionBtn');
+const allBtn = document.getElementById('allBtn');
 
-const fetchX = async () => {
-    await fetch("https://restcountries.eu/rest/v2/all")
+const fetchAll = async () => {
+    await fetch("https://restcountries.eu/rest/v2/all?_limit=10")
    .then(res => {
      //tikrinam response, kad galetum catch error.
       console.log(res);
@@ -33,4 +36,31 @@ const fetchX = async () => {
      console.log(err);
    })
  }
- fetchX();
+
+allBtn.addEventListener('click', fetchAll);
+
+ const fetchRegion = async () => {
+     const regionSelected = selectRegion.value;
+     try{
+        const response = await fetch(`https://restcountries.eu/rest/v2/region/${regionSelected}`);
+        const data = await response.json();
+        console.log(data);
+        const salys = data.map(salis => {
+     
+            return `<div class="col pb-2">
+                    <div class="card" style="width: 18rem;">
+                      <img src="${salis.flag}" width="200px" height="150px" class="card-img-top" alt="flag">
+                      <div class="card-body">
+                        <h5 class="card-title">${salis.name}</h5>
+                        <h6 class="card-subtitle mb-2 text-muted">${salis.capital}</h6>
+                     </div>
+                    </div>
+                    </div>`
+          }).join('');
+          display.insertAdjacentHTML('afterbegin', salys);
+     } catch (err) {
+         console.log(err)
+     }
+ }
+ regionBtn.addEventListener('click', fetchRegion);
+
