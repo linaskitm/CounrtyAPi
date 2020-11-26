e8a6163e1de1b33202efc7ca63c82e5b6cf3c2e9
Linas Pelenis <lpelenis@gmail.com>
2020-11-26 16:15:43 +0200

    one counrty fech, extract data from inner arrays

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ const regionBtn = document.getElementById('regionBtn');
 const allBtn = document.getElementById('allBtn');
 const countryOption = document.getElementById('countryOption');
 const countries = document.getElementById('countries');
+const oneCounrtyBtn = document.getElementById('oneCounrtyBtn'); 
 
 
 const fetchAll = async () => {
@@ -68,7 +69,7 @@ allBtn.addEventListener('click', fetchAll);
  regionBtn.addEventListener('click', fetchRegion);
 
  let defaultOption = document.createElement('option');
- defaultOption.text = 'Choose Country';
+ defaultOption.value = 'Choose Country';
  countries.appendChild(defaultOption);
  countries.selectedIndex = 0;
 
@@ -88,10 +89,70 @@ allBtn.addEventListener('click', fetchAll);
             for(let i =0; i < data.length; i++) {
                     option.value = data[i].name;    
             }
-            countries.appendChild(option); 
+            countries.appendChild(option);
     } catch (err) {
         console.log('Error Bl:', err);
     }
     }
+    
 
 countryOption.addEventListener('keyup', fetchOptions, false);
+
+const fecthCounrty = async () => {
+    const inputValue = countryOption.value;
+    try {
+        const response = await fetch(`https://restcountries.eu/rest/v2/name/${inputValue}`);
+        const data = await response.json();
+        display.innerHTML = "";
+        data.forEach(salis => {
+          console.log(salis);
+          const div = document.createElement('div');
+          div.setAttribute('class', 'jumbotron');
+          display.appendChild(div);
+
+          const img = document.createElement('img');
+          img.setAttribute('class', 'img-thumbnail');
+          img.src = salis.flag;
+          div.appendChild(img);
+
+          const name = document.createElement('h3');
+          name.setAttribute('class', 'display-4');
+          name.innerText = salis.name;
+          div.appendChild(name);
+
+          const currencyList = document.createElement('ul');
+          currencyList.innerText = 'currencies';
+          div.appendChild(currencyList)
+          for(let c in salis.currencies) {
+            const currency = document.createElement('li')
+            currency.innerHTML = salis.currencies[c].name;
+            currencyList.appendChild(currency);
+          }
+
+          const borderList = document.createElement('ul');
+          borderList.innerText = 'Border';
+          div.appendChild(borderList)
+          for(let b in salis.borders) {
+            const border = document.createElement('li')
+            border.innerHTML = salis.borders[b];
+            borderList.appendChild(border);
+          }
+
+        })
+        
+    } catch (error) {
+        console.log('Error Bl:' , error)
+        
+    }
+}
+function resetValue() {
+  countryOption.value = '';
+}
+
+
+oneCounrtyBtn.addEventListener('click', function(e){
+  e.preventDefault();
+  fecthCounrty();
+  resetValue();
+
+});
